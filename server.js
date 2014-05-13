e60ffcae2a93e6f6613331e2d5dabd5b0337b63c
David Simon <me@davidsimon.ch>
2014-05-13 13:12:33 +0200

    [Feature] Express Server to serve App + API // [Update] Store uses deepcopy

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
+// Config
 var config = {
     screenDir: '/screenshots/screenshots/' // the nested directory hackfixes a bug/feature(?) in automator where the file is placed outside the actual dir
   , cwd: __dirname
   , workflowFile: 'print_screenshot.workflow'
-  , port: 8000
+  , port: process.env.PORT || 8000
 };
 
+// Dependencies
 var mkdirp = require('mkdirp')
   , exec = require('child_process').exec
-  , print = require('./lib/print')(exec, mkdirp, config)
-  //, server = require('./lib/server')()
-  , store = require('./lib/store')();
+  , express = require('express')
+  , bodyParser = require('body-parser')
+  , deepcopy = require('deepcopy')
+  , io = require('socket.io')
+  ;
 
+// Modules
+var print = require('./lib/print')(exec, mkdirp, config)
+  , store = require('./lib/store')()
+  , server = require('./lib/server')(express, bodyParser, store, config)
+  ;
 
 // Inital state
 store.save('state', {
     currentTerm: null,
     subTerms: []
 });
+
+io.listen(server);
+
 
 // Test
 print.print();
